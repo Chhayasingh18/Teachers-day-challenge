9aa3ba51c43a4a0debecc2a6ee4f11b3c070d735
Chhaya Singh <166850490+Chhayasingh18@users.noreply.github.com>
2025-08-02 22:35:52 +0530

    Create  Petya_and_Strings.java

---
+++ Day09/Petya_and_Strings.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PetyaAndStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String first = scanner.nextLine();
+        String second = scanner.nextLine();
+        
+        first = first.toLowerCase();
+        second = second.toLowerCase();
+        
+        int result = first.compareTo(second);
+        
+        if (result < 0) {
+            System.out.println("-1");
+        } else if (result > 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
