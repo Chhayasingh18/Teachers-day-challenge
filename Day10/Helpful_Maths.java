07662102fef672251864f3ad55936d37df575f9b
Chhaya Singh <166850490+Chhayasingh18@users.noreply.github.com>
2025-08-03 20:57:01 +0530

    Update Helpful_Maths.java

--- Day10/Helpful_Maths.java
+++ Day10/Helpful_Maths.java
@@ -1,1 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class HelpfulMaths {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String[] numbers = s.split("\\+");
+        Arrays.sort(numbers);
+        System.out.println(String.join("+", numbers));
+    }
+}
