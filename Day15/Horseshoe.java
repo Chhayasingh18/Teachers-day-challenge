bf04921eaaa3e19289be18804515822826bc2ebf
Chhaya Singh <166850490+Chhayasingh18@users.noreply.github.com>
2025-08-09 00:22:21 +0530

    Create Horseshoe.java

---
+++ Day15/Horseshoe.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<Integer> colors = new HashSet<>();
+        for (int i = 0; i < 4; i++) {
+            colors.add(scanner.nextInt());
+        }
+        System.out.println(4 - colors.size());
+    }
+}
