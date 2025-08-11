d0a10ca5d979377a24f6f7c6b2e4ee9f0938b09b
Chhaya Singh <166850490+Chhayasingh18@users.noreply.github.com>
2025-08-11 20:27:17 +0530

    Create Cheap_Travel.java

---
+++ Day17/Cheap_Travel.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class CheapTravel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        int cost1 = n * a; 
+        int cost2 = ((n + m - 1) / m) * b; 
+        int cost3 = (n / m) * b + (n % m) * a; 
+        
+        int minCost = Math.min(cost1, Math.min(cost2, cost3));
+        System.out.println(minCost);
+    }
+}
